4c38434c69fdd3fc63b9664e6d2b768f81d4c619
道化師 <wraith13@hotmail.co.jp>
2020-03-07 21:48:08 +0900

    🛠 実装 ( WIP )

--- index.ts
+++ index.ts
@@ -7,9 +7,13 @@ export module Slack
         client_id: string;
         client_secret: string;
     }
+    type UserId = string;
+    type TeamId = string;
+    type ChannelId = string;
+    type UnixTime = number;
     export interface User
     {
-        id: string;
+        id: UserId;
         scope: string;
         access_token: string;
         token_type: string;
@@ -28,24 +32,55 @@ export module Slack
     }
     export interface Team
     {
-        id: string;
+        id: TeamId;
         name: string;
         domain: string;
         email_domain: string;
         icon: Icon;
     }
-    export interface Profile extends Icon
+    export interface Label
     {
-        avatar_hash: string;
+        value: string;
+        creator: UserId;
+        last_set: UnixTime;
+    }
+    export interface Channel
+    {
+        id: ChannelId;
+        name: string;
+        is_channel: true;
+        created: UnixTime;
+        is_archived: boolean;
+        is_general: boolean;
+        unlinked: number;
+        creator: UserId;
+        name_normalized: string;
+        is_shared: boolean;
+        is_org_shared: boolean;
+        is_member: boolean;
+        is_private: boolean;
+        is_mpim: boolean;
+        members: UserId[];
+        topic: Label;
+        purpose: Label;
+        previous_names: string[];
+        num_members: number;
+    }
+    export interface Status
+    {
         status_text: string;
         status_emoji: string;
-        status_expiration: number;
+        status_expiration: UnixTime;
+    }
+    export interface Profile extends Status, Icon
+    {
+        avatar_hash: string;
         real_name: string;
         display_name: string;
         real_name_normalized: string;
         display_name_normalized: string;
         email: string;
-        team: string;
+        team: TeamId;
     }
 }
 
