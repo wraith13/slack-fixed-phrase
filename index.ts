1921a6a25a4891cfe687d3ae20a9d15bd9213482
道化師 <wraith13@hotmail.co.jp>
2020-03-11 19:24:26 +0900

    🛠 実装 ( WIP )

--- index.ts
+++ index.ts
@@ -11,6 +11,7 @@ export module Slack
     type UserId = string;
     type TeamId = string;
     type ChannelId = string;
+    type BotId = string;
     type AccessToken = string;
     type UnixTime = number;
     export interface User
@@ -22,14 +23,21 @@ export module Slack
     }
     export interface Icon
     {
-        image_34: string;
-        image_44: string;
-        image_68: string;
-        image_88: string;
-        image_102: string;
-        image_132: string;
-        image_230: string;
-        image_original: string;
+        image_24?: string;
+        image_32?: string;
+        image_34?: string;
+        image_44?: string;
+        image_48?: string;
+        image_68?: string;
+        image_72?: string;
+        image_88?: string;
+        image_102?: string;
+        image_132?: string;
+        image_192?: string;
+        image_230?: string;
+        image_512?: string;
+        image_original?: string;
+
 
     }
     export interface Team
@@ -120,7 +128,61 @@ export module Slack
         minamo.http.getJson ( `https://slack.com/api/team.info?token=${ token }` );
     export const channelsList = async ( token: AccessToken ): Promise<{ ok: boolean, channels: Channel[] }> =>
         minamo.http.getJson ( `https://slack.com/api/channels.list?token=${ token }`);
-    export const emojiList = async ( token: AccessToken, limit: string ) => minamo.http.get ( `https://slack.com/api/emoji.list?token=${ token }&limit=${ limit }` );
+    export const emojiList = async ( token: AccessToken, limit: string ): Promise<{ ok: boolean, emoji: { [name: string]: string } }> =>
+        minamo.http.getJson ( `https://slack.com/api/emoji.list?token=${ token }&limit=${ limit }` );
+    export const chatPostMessage = async (
+            token: AccessToken,
+            data:
+            {
+                channel: ChannelId,
+                text: string,
+            }
+        ):
+        Promise<{
+            ok: boolean,
+            channel: ChannelId,
+            ts: string,
+            message:
+            {
+                bot_id: BotId,
+                type: string,
+                text: string,
+                user: UserId,
+                ts: string,
+                team: TeamId,
+                bot_profile:
+                {
+                    id: BotId,
+                    deleted: boolean,
+                    name: string,
+                    updated: UnixTime,
+                    app_id: AppId,
+                    icons: Icon,
+                    team_id: TeamId,
+                }
+            },
+        }> =>
+        minamo.http.postJson
+        (
+            `https://slack.com/api/chat.postMessage`,
+            JSON.stringify ( data ),
+            { Authorization: `Bearer ${token}` }
+        );
+    export const usersProfileSet = async (
+            token: AccessToken,
+            data: { profile: Status, }
+        ):
+        Promise<{
+            ok: boolean,
+            username: string,
+            profile: Profile,
+        }> =>
+        minamo.http.postJson
+        (
+            `https://slack.com/api/users.profile.set`,
+            JSON.stringify ( data ),
+            { Authorization: `Bearer ${token}` }
+        );
 }
 
 export module SlackFixedPhrase
